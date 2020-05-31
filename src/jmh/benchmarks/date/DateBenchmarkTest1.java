45d3a1feaccf00985aeedd879e52f3287317609f
如梦技术 <596392912@qq.com>
2020-05-31 21:31:38 +0800

    :arrow_up: 依赖升级，测试 mica 2.x 版本性能

--- src/jmh/benchmarks/date/DateBenchmarkTest1.java
+++ src/jmh/benchmarks/date/DateBenchmarkTest1.java
@@ -1,5 +1,6 @@
 package jmh.benchmarks.date;
 
+import cn.hutool.core.date.DateUtil;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.results.format.ResultFormatType;
@@ -8,8 +9,6 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -22,12 +21,11 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Thread)
 public class DateBenchmarkTest1 {
-	public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault());
 	public static final FastDateFormat FAST_DATE_FORMAT = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss");
 
 	@Benchmark
-	public String toInstantFormat() {
-		return DATETIME_FORMATTER.format(new Date().toInstant());
+	public String micaDataFormat() {
+		return DateUtil.formatDateTime(new Date());
 	}
 
 	@Benchmark
