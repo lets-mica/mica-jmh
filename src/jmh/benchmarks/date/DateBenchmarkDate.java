664326539a31684cdf5359ba8d04c7420ec34b6c
如梦技术 <596392912@qq.com>
2020-06-05 16:24:26 +0800

    :sparkles: 测试不同方式转换到 date 的性能。

---
+++ src/jmh/benchmarks/date/DateBenchmarkDate.java
@@ -0,0 +1,66 @@
+package jmh.benchmarks.date;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * java 各种转 date 压测
+ *
+ * @author L.cm
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class DateBenchmarkDate {
+
+	@Benchmark
+	public Date calendar() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setLenient(false);
+		calendar.set(Calendar.YEAR, 1970);
+		calendar.set(Calendar.MONTH, 1);
+		calendar.set(Calendar.DATE, 1);
+		calendar.set(Calendar.SECOND, 1);
+		calendar.set(Calendar.MINUTE, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 1);
+		return calendar.getTime();
+	}
+
+	@Benchmark
+	public Date localDate() {
+		Instant instant = LocalDate.now().atStartOfDay()
+			.atZone(ZoneOffset.UTC)
+			.toInstant();
+		return Date.from(instant);
+	}
+
+	@Benchmark
+	public Date localTime() {
+		LocalTime lt = LocalTime.now();
+		Instant instant = lt.atDate(LocalDate.EPOCH)
+			.atZone(ZoneOffset.UTC)
+			.toInstant();
+		return Date.from(instant);
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opts = new OptionsBuilder()
+			.include(DateBenchmarkDate.class.getSimpleName())
+			.warmupIterations(5)
+			.measurementIterations(5)
+			.jvmArgs("-server")
+			.forks(1)
+			.resultFormat(ResultFormatType.TEXT)
+			.build();
+		new Runner(opts).run();
+	}
+}
